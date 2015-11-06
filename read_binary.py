187c110fa7b68bf288106fc6e6010dc0b271d896
Pedro Paiva <pedroarbs@gmail.com>
2015-11-05 22:22:09 -0200

    Adicionado file para transferencia de arquivos

---
+++ read_binary.py
@@ -0,0 +1,36 @@
+# -*- coding: cp1252 -*-
+import os
+import zipfile
+
+# colocar aqui o diretório do arquivo usando barra dupla (\\)
+os.chdir('C:\\Users\\Usuário\\Desktop\\ITA\\ELE\\4º Semestre\\Projeto EEA-47\\Leitura_Arquivos')
+
+# lê o nome e a extebsão do arquivo
+name = raw_input("Digite o nome do arquivo que deseja enviar: ")
+ext = raw_input("Digite a extensão do arquivo: ")
+arq = name + "." + ext
+
+# cria um zip e adiciona o arquivo
+zf = zipfile.ZipFile(name + ".zip", 'w')
+zf.write(arq)
+zf.close()
+
+# lê o arquivo zipado como binário
+with open(name + ".zip",'rb') as f:
+    data = f.read()
+os.remove(name + ".zip")
+
+data_byte = bytearray(data)
+N = len(data_byte)
+
+
+# simulando a recepção
+recebido_byte = bytearray(N)
+for i in range(0,N):
+    recebido_byte[i] = data_byte[i]
+
+recebido = str(recebido_byte)
+
+with open(name + "_saida.zip",'wb') as g:
+    g.write(recebido)
+
